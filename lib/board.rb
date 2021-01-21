56a1a02d8198286274b0a2a527978fca5b59fadd
Constance Mann <connie.mann@yahoo.com>
2021-01-21 18:09:53 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,40 @@
 # Define the variable board below.
+board = "Board"
+board = 'Board.new'
+row = ["", "", "", "", "", "", "", "", ""]
+row = [""]
+row = [" ", " ", " "]
+row[0]
+row[1]
+row[2]
+row[1] = "X"
+row = [" ", " ", " "]
+row [0]
+row [1]
+row [2]
+row [1] = "X"
+row = [" ", " ", " "]
+row [0]
+
+row = [" ", " ", " "]
+
+# Position 1: Left
+row[0]
+
+# Position 2: Middle
+row[1]
+
+# Position 3: Right
+row[2]
+
+# Move X to the Position 2, Middle
+row[1] = "X"
+row #=> [" ", "X", " "]
+['1, 2, 3, 4, 5, 6, 7, 8, 9']
+["1, 2, 3, 4, 5, 6, 7, 8, 9"]
+board = ["1, 2, 3, 4, 5, 6, 7, 8, 9"]
+board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+board = [""]
+board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+board = ["1"]
+board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
